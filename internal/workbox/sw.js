4b1a9e4e015424615a81b77a030686cc85c6e9d6
Kayou W. <kayou-w-@outlook.com>
2024-03-11 23:47:14 -0400

    revert import in sw.js

--- internal/workbox/sw.js
+++ internal/workbox/sw.js
@@ -1,22 +1,22 @@
 // This code executes in its own worker or thread
 // scope: '/'
 
-import { registerRoute, Route, setDefaultHandler } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate, NetworkOnly, NetworkFirst } from 'workbox-strategies';
-import { ExpirationPlugin } from 'workbox-expiration';
-// import {offlineFallback} from 'workbox-recipes'; 
-import { warmStrategyCache } from 'workbox-recipes';
-import { matchPrecache, precacheAndRoute, precaching } from 'workbox-precaching';
-// import {BackgroundSyncPlugin} from 'workbox-background-sync'; // queue up failed requests and retry them when future sync events fire
-import { cacheNames, setCacheNameDetails } from 'workbox-core';
+// import { registerRoute, Route, setDefaultHandler } from 'workbox-routing';
+// import { CacheFirst, StaleWhileRevalidate, NetworkOnly, NetworkFirst } from 'workbox-strategies';
+// import { ExpirationPlugin } from 'workbox-expiration';
+// // import {offlineFallback} from 'workbox-recipes'; 
+// import { warmStrategyCache } from 'workbox-recipes';
+// import { matchPrecache, precacheAndRoute, precaching } from 'workbox-precaching';
+// // import {BackgroundSyncPlugin} from 'workbox-background-sync'; // queue up failed requests and retry them when future sync events fire
+// import { cacheNames, setCacheNameDetails } from 'workbox-core';
 
-// self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js');
-// const { registerRoute, Route, setDefaultHandler, setCatchHandler } = workbox.routing;
-// const { CacheFirst, StaleWhileRevalidate, NetworkOnly, NetworkFirst } = workbox.strategies;
-// const { ExpirationPlugin } = workbox.expiration;
-// const { warmStrategyCache } = workbox.recipes;
-// const { matchPrecache, precacheAndRoute, precaching } = workbox.precaching;
-// const { cacheNames, setCacheNameDetails } = workbox.core;
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js');
+const { registerRoute, Route, setDefaultHandler, setCatchHandler } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate, NetworkOnly, NetworkFirst } = workbox.strategies;
+const { ExpirationPlugin } = workbox.expiration;
+const { warmStrategyCache } = workbox.recipes;
+const { matchPrecache, precacheAndRoute, precaching } = workbox.precaching;
+const { cacheNames, setCacheNameDetails } = workbox.core;
 
 
 const SW_VERSION = 'plu-quiz-1.0.0';
